edea8b9e5d468ead82938cdc304842b72a7aff38
VKotelnykova <10515117@stud.op.edu.ua>
2024-05-20 17:43:11 +0300

    Initial commit

---
+++ src/main/java/com/example/demoBott/DemoBottApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoBott;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBottApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBottApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoBott/DemoBottApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoBott;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBottApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
