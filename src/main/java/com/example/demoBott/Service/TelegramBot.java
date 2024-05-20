980e17a6e4ce7589475445121eefa2bcb2bda2ff
VKotelnykova <10515117@stud.op.edu.ua>
2024-05-20 21:51:53 +0300

    Added buttons for main menu

--- src/main/java/com/example/demoBott/Service/TelegramBot.java
+++ src/main/java/com/example/demoBott/Service/TelegramBot.java
@@ -1,11 +1,16 @@
 package com.example.demoBott.Service;
 
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import java.util.List;
+import java.util.ArrayList;
 import com.example.demoBott.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 
@@ -35,15 +40,12 @@ public class TelegramBot extends TelegramLongPollingBot {
             String messageText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
 
-            switch (messageText) {
-                case "/start":
+            if (messageText.equals("/start")) {
+                sendStartKeyboard(update.getMessage().getChatId());
 
-                        startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
-                    break;
-                default:
-
-                        sendMessage(chatId, "Sorry, not working");
-
+                sendMenu(chatId);
+            } else {
+                sendMessage(chatId, "Sorry,command not working");
             }
         }
     }
@@ -65,6 +67,59 @@ public class TelegramBot extends TelegramLongPollingBot {
             execute(message);
         }
         catch (TelegramApiException e){
+            log.error("Error occurred:" + e.getMessage());
+        }
+    }
+    private void sendStartKeyboard(long chatId) {
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        keyboardMarkup.setResizeKeyboard(true);
+
+        KeyboardRow row = new KeyboardRow();
+        row.add("/start"); // Добавляем кнопку "Start"
+
+        List<KeyboardRow> keyboard = new ArrayList<>();
+        keyboard.add(row);
+
+        keyboardMarkup.setKeyboard(keyboard);
+
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText("Welcome to the bot! Press the button below to start.");
+        message.setReplyMarkup(keyboardMarkup);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred:" + e.getMessage());
+        }
+    }
+
+    private void sendMenu(long chatId){
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+        keyboardMarkup.setResizeKeyboard(true);
+        KeyboardRow row1 = new KeyboardRow();
+        row1.add("Цілі");
+
+        KeyboardRow row2 = new KeyboardRow();
+        row2.add("Мотивація");
+
+        KeyboardRow row3 = new KeyboardRow();
+        row3.add("Колесо фортуни");
+
+
+        List<KeyboardRow> keyboard = new ArrayList<>();
+        keyboard.add(row1);
+        keyboard.add(row2);
+        keyboard.add(row3);
+
+        keyboardMarkup.setKeyboard(keyboard);
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText("Here is the menu:");
+        message.setReplyMarkup(keyboardMarkup);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
             log.error("Error occurred:" + e.getMessage());
         }
     }
